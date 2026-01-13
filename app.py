fcb8ca24219a49cbc4f3576300f190fcdbfb85f6
Ayushman2005 <karayushman736@gmail.com>
2026-01-13 22:09:35 +0530

    13/01/2026

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ db_config = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", ""),
-    "database": os.getenv("DB_NAME", "society_db")
+    "database": os.getenv("DB_NAME", "")
 }
 
 def get_db_connection():
