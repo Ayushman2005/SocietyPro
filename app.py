0b8d2e48a8ade2d0ca270a89aa88d1d440932f45
Ayushman2005 <karayushman736@gmail.com>
2026-02-16 18:36:32 +0530

    16/02/2026

--- app.py
+++ app.py
@@ -1291,4 +1291,4 @@ def payment_success(bill_id):
 
     return render_template('user/payment_success.html', bill_id=bill_id)
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
